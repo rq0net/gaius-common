4fe72ffc98dba0278b1abee6c0da43b5c6434190
victor <Ion Atman>
2021-11-12 14:30:08 +0800

    storrage

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="51ecxPKZfPJl7uIPtWzBxvhNBVUfep1YTOXZ6zIkR8SM9wCIZuwG8LuI0VXaRMZm",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
